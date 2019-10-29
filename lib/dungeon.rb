d12b521875e8421661c554da421552c7b7f07534
Andrew Baird <andrew.baird@housecallpro.com>
2019-10-29 13:01:55 -0700

    use 0 as default

--- lib/dungeon.rb
+++ lib/dungeon.rb
@@ -2,15 +2,16 @@ class Dungeon
   def initialize(rows, cols)
     @rows = rows
     @columns = cols
-    @wall_arr = Array.new(rows){Array.new(cols)}
+    @wall_arr = Array.new(rows){Array.new(cols, 0)}
   end
 
   def draw
     drawing = ""
+
     @wall_arr.each do |row|
       drawing += "\n"
       row.each do |column|
-        drawing += '0'
+        drawing += column.to_s
       end
     end
 
