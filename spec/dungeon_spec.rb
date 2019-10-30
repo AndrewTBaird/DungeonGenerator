21729cd47736ff844b4a45c7020fe857cfabf190
Andrew Baird <andrew.baird@housecallpro.com>
2019-10-29 21:05:55 -0700

    add test file

--- spec/dungeon_spec.rb
+++ spec/dungeon_spec.rb
@@ -2,16 +2,6 @@ require "spec_helper"
 require "dungeon"
 
 describe Dungeon do
-
-  describe "#generate_tunnels" do
-    let(:dungeon) { Dungeon.new(40, 40, 8, 100) }
-
-    it "generates tunnels" do
-      dungeon.generate_tunnels
-      print dungeon.draw
-    end
-  end
-
   describe "#draw_dungeon" do
     let(:dungeon) { Dungeon.new(3, 2, 2, 1) }
     it { expect(dungeon.draw).to eq("\nXX\nXX\nXX") }
